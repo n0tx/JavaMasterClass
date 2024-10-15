d2c81c354a78f29fcdf5ce011f887e6922e03fdb
n0tx <rcandra91@msn.com>
2024-10-16 03:10:15 +0700

    operator

---
+++ src/main/java/section4/Operator.java
@@ -0,0 +1,78 @@
+package section4;
+
+public class Operator {
+    /*
+    public static void main(String[] args) {
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result + " // result");
+
+        int previousResult = result;
+        System.out.println("previousResult = result" + " // previousResult == result");
+
+        result = result - 1;
+
+        System.out.println("result - 1 = " + result + " // result");
+
+        previousResult = result;
+
+        result = result * 100;
+        System.out.println("previousResult = result" + " // previousResult == result");
+
+        System.out.println("previousResult = " + previousResult + " // previousResult");
+    }
+     */
+    public static void main(String[] args) {
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+
+        result = result - 1;
+        System.out.println(previousResult + " - 1 = " + result);
+
+        previousResult = result;
+
+        result = result * 10;
+        System.out.println(previousResult + " * 10 = " + result);
+
+        previousResult = result;
+
+        result = result / 5;
+        System.out.println(previousResult + " / 5 = " + result);
+
+        previousResult = result;
+        result = result % 3;
+        System.out.println(previousResult + " % 3 = " + result);
+
+        previousResult = result;
+        result = result + 1;
+        System.out.println("Result is now " + result);
+        result++;
+        System.out.println("Result is now " + result);
+        result--;
+        System.out.println("Result is now " + result);
+
+        result += 2;
+        System.out.println("Result is now " + result);
+        result *= 10;
+        System.out.println("Result is now " + result);
+        result -= 10;
+        System.out.println("Result is now " + result);
+        result /= 10;
+        System.out.println("Result is now " + result);
+
+
+        boolean isAlien = false;
+        if (isAlien == true)
+            System.out.println("It is not an alien!");
+
+        int topScore = 80;
+        if (topScore < 100)
+            System.out.println("You got the high score!");
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100)
+            System.out.println("Greater than second top score and less then 100");
+    }
+
+}
